5c920492be8e9126ddf290076b53d2c3cedab577
Fergus Cooper <fcooper8472@gmail.com>
2020-06-04 22:05:43 +0100

    try again to make flake8 fail

--- ci_demo/functionality.py
+++ ci_demo/functionality.py
@@ -1,12 +1,13 @@
 import numbers
 
+
 def greet(name=""):
     return f"Hello {name}"
 
 
 def minimum(*args):
     if not any([isinstance(arg, numbers.Real) for arg in args]):
-        return
+            return
 
     the_min = float("inf")
     for arg in args:
