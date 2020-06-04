bdc04f91b4bba212d92f9b068498c75f6726786e
Fergus Cooper <fcooper8472@gmail.com>
2020-06-04 22:55:20 +0100

    Add some simple docstrings

--- ci_demo/functionality.py
+++ ci_demo/functionality.py
@@ -2,10 +2,22 @@ import numbers
 
 
 def greet(name=""):
+    """
+    A function that takes a name and returns a greeting.
+
+    :param name: the name to greet
+    :return: the greeting
+    """
     return f"Hello {name}"
 
 
 def minimum(*args):
+    """
+    A function taking some arguments and returning the minimum number among the arguments.
+
+    :param args: the arguments
+    :return: the minimum
+    """
     if not any([isinstance(arg, numbers.Real) for arg in args]):
             return
 
